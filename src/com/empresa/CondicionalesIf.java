b4fa42e80d0e3ccf6834d8e4da6052db7a4e7813
Padilla <jose.padilla@softwareone.com>
2025-11-05 00:47:59 -0500

    tercer commit  04-11-2025 - creación de clase CondicionalesIf

---
+++ src/com/empresa/CondicionalesIf.java
@@ -0,0 +1,42 @@
+package com.empresa;
+
+public class CondicionalesIf {
+
+	public static void main(String[] args) {
+		String var1 ="Hola";
+		String var2 =" Equipo";
+		
+		int a =3000;
+		int b =200;
+		
+		/*if (a>10000) {
+			System.out.println("A es mayor que 1000");
+		}
+		else 
+			
+		{
+			
+			System.out.println("A no es mayor que 1000");
+		}*/
+		
+		if(a==100) {
+			System.out.println("A es 100");
+		}
+		
+		else if(a==200) {
+			System.out.println("A es 200");
+		}
+		
+		else if(a==300) {
+			System.out.println("A es 300");
+		}
+		
+		else {
+			System.out.println("A no es igual a ninguno de los números");
+		}
+		
+		
+
+	}
+
+}
