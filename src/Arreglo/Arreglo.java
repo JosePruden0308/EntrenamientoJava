ab132fcd6106e6176495337fcc1886a9b0627801
Padilla <jose.padilla@softwareone.com>
2025-12-10 00:40:00 -0500

    Commit arreglos

---
+++ src/Arreglo/Arreglo.java
@@ -0,0 +1,30 @@
+package Arreglo;
+
+public class Arreglo {
+
+	
+	
+	public static void main(String[] args) {
+		
+		//Definición del arreglo
+		int arreglo1[]=new int[10];
+		
+		//Definición de valores
+		arreglo1[0]=12;
+		arreglo1[1]=20;
+		arreglo1[2]=18;
+		arreglo1[3]=92;
+		arreglo1[4]=62;
+		arreglo1[5]=25;
+		arreglo1[6]=100;
+		arreglo1[7]=7;
+		arreglo1[8]=81;
+		arreglo1[9]=9;
+		
+		//Acceder a los valores del arreglo
+		System.out.println(arreglo1[4]);
+	
+		
+	}
+
+}
