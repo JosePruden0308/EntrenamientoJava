ff38413979f2b5f90eda898efc1307a02c70e1e9
Padilla <jose.padilla@softwareone.com>
2025-12-16 23:42:54 -0500

    Commit para actualizar clase de manejo de Excel

--- src/Utilities/WriteExcelFile.java
+++ src/Utilities/WriteExcelFile.java
@@ -22,6 +22,57 @@ public class WriteExcelFile {
 			
 			//Se agregan los datos al mapa (Cada fila es un arreglo de objetos)
 			dataset.put("1", new Object[] {"ID","NOMBRE","CIUDAD"});
+			dataset.put("2", new Object[] {"01","Jeremíass","Medellín"});
+			dataset.put("3", new Object[] {"02","Joel","Medellín"});
+			dataset.put("4", new Object[] {"03","Judith","Medellín"});
+			dataset.put("5", new Object[] {"04","Carlos","Riohacha"});
+			dataset.put("6", new Object[] {"05","Carmen","Planeta Rica"});
+			dataset.put("7", new Object[] {"06","Paula","Montería"});
+			
+			//Se obtiene el conjunto de claves del mapa
+			Set<String> set=dataset.keySet();
+			int rownum=0;
+			
+			//Se itera sobre cada fila de datos
+			for(String key : set) {
+				//Se crea una nueva fila en la hoja
+				Row row =sampleSheet.createRow(rownum++);
+				
+				//Se obtienen los datos correspondientes a la fila
+				Object[]data=dataset.get(key);
+				int cellNum=0;
+				
+				//Se itera sobre los valores de la fila para crear celdas
+				for (Object value : data) {
+					//Se crea una nueva celda
+					Cell cell=row.createCell(cellNum++);
+					
+					//Se verifica el tipo de dato y se asigna el valor de la celda
+					if(value instanceof String) {
+						cell.setCellValue((String)value);
+					}
+					else if(value instanceof Integer){
+						cell.setCellValue((Integer)value);
+					}
+					
+				}
+			}
+			
+			//Se escribe el archivo de Excel en el disco duro
+			try {
+				//Define la ruta y nombre del archivo de salida
+				FileOutputStream writeFile=new FileOutputStream("sampleTest.xlsx");
+				
+				//Escribe el contenido del workbook en el archivo
+				wordbook.write(writeFile);
+				
+				//Cierra el flujo del archivo
+				writeFile.close();
+				System.out.println("El archivo se creó exitosamente");
+			}
+			catch(FileNotFoundException e) {
+				e.printStackTrace();
+			}
 			
 			
 		}
